d9527af25a2a3775717206f53090752461edb47a
Abhijeet Anand <69691394+aabhijeet011@users.noreply.github.com>
2021-06-06 23:34:22 +0530

    files Added

---
+++ HappyBirthday/README.md
@@ -0,0 +1,5 @@
+# HappyBirthday
+- It's just a web page for birthday wishes.
+- EveryOne want's to be unique so that's what I was trying to do..😀
+- You can clone it and modify in your way to wish.
+## ThankYou 📣
---
+++ HappyBirthday/index.js
@@ -0,0 +1,10 @@
+// var x=getElementById('player');
+// x.autoplay();
+function play() {
+        var audio = document.getElementById("audio");
+        audio.play();
+      }
+function pause(){
+     var audio= document.getElementById("audio");
+     audio.pause()
+}
